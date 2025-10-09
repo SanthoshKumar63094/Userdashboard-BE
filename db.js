4b590601f0d94a28b2be7acd2d960d0dea30e0be
SanthoshKumar63094 <palanisanthoshkumar123@gmail.com>
2025-10-09 12:26:19 +0530

    second commit

--- db.js
+++ db.js
@@ -5,11 +5,8 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required for Neon
 });
 
 export const query = (text, params) => pool.query(text, params);
